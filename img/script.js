b10ebf1043ddbd50dc7648fefaab59819a7e4c48
boyoungbk <boyoungbk@gmail.com>
2022-07-14 10:27:39 +0900

    html로 수정함

--- img/script.js
+++ img/script.js
@@ -1,7 +1,5 @@
-var container = document.getElementById('map');
-var options = {
-	center: new kakao.maps.LatLng(33.450701, 126.570667),
-	level: 3
-	};
+//지도를 삽입할 HTML 요소 또는 HTML 요소의 id를 지정합니다.
+var mapDiv = document.getElementById('map'); // 'map'으로 선언해도 동일
 
-var map = new kakao.maps.Map(container, options);
+//옵션 없이 지도 객체를 생성하면 서울 시청을 중심으로 하는 16 레벨의 지도가 생성됩니다.
+var map = new naver.maps.Map(mapDiv);
