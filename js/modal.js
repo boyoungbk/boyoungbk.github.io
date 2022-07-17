14608c03f1ce2b1f9ef852b21a5ff55148752691
boyoungbk <boyoungbk@gmail.com>
2022-07-17 20:09:52 +0900

    copy

--- js/modal.js
+++ js/modal.js
@@ -7,3 +7,13 @@ function openmodal() {
   var mdl = document.getElementById("myModal");
  mdl.style.display="none"
  };
+
+ function openmodal2() {
+  var mdl = document.getElementById("myModal2");
+ mdl.style.display="block"
+ };
+ 
+ function closemodal2() {
+  var mdl = document.getElementById("myModal2");
+ mdl.style.display="none"
+ };
