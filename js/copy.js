c018653a567d6a014ff06868cd866c8a1f383ce8
boyoungbk <boyoungbk@gmail.com>
2022-07-17 23:20:56 +0900

    popup

--- js/copy.js
+++ js/copy.js
@@ -1,9 +1,38 @@
-function copyToClipBoard() {
+const btn_copy = document.getElementsByClassName("btn_copy")
+const div_copy = document.getElementsByClassName("textArea")
 
-  var content = document.getElementById('textArea');
-  
-  content.select();
-  document.execCommand('copy');
+btn_copy[0].onclick = () => {
+  window.navigator.clipboard.writeText(div_copy[0].textContent).then(() => {
+    alert(div_copy[0].textContent + " 복사 완료");
+  });
+}
 
-  alert("Copied!");
+btn_copy[1].onclick = () => {
+  window.navigator.clipboard.writeText(div_copy[1].textContent).then(() => {
+    alert(div_copy[1].textContent + " 복사 완료");
+  });
+}
+
+btn_copy[2].onclick = () => {
+  window.navigator.clipboard.writeText(div_copy[2].textContent).then(() => {
+    alert("🤵🏻" + div_copy[2].textContent + " 복사 완료!");
+  });
+}
+
+btn_copy[3].onclick = () => {
+  window.navigator.clipboard.writeText(div_copy[3].textContent).then(() => {
+    alert(div_copy[3].textContent + " 복사 완료");
+  });
+}
+
+btn_copy[4].onclick = () => {
+  window.navigator.clipboard.writeText(div_copy[4].textContent).then(() => {
+    alert("" + div_copy[4].textContent + " 복사 완료");
+  });
+}
+
+btn_copy[5].onclick = () => {
+  window.navigator.clipboard.writeText(div_copy[5].textContent).then(() => {
+    alert("👰🏻‍♀️" + div_copy[5].textContent + " 복사 완료!");
+  });
 }
