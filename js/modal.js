87c12adfd57d1b34e9ffb9b7c44ea82ba2ec4163
boyoungbk <boyoungbk@gmail.com>
2022-07-17 21:18:37 +0900

    span

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,19 @@
 function openmodal() {
   var mdl = document.getElementById("myModal");
- mdl.style.display="block"
+  mdl.style.display="block"
  };
  
  function closemodal() {
   var mdl = document.getElementById("myModal");
- mdl.style.display="none"
+  mdl.style.display="none"
  };
 
  function openmodal2() {
-  var mdl = document.getElementById("myModal2");
- mdl.style.display="block"
+  var mdl2 = document.getElementById("myModal2");
+  mdl2.style.display="block"
  };
  
  function closemodal2() {
-  var mdl = document.getElementById("myModal2");
- mdl.style.display="none"
+  var mdl2 = document.getElementById("myModal2");
+  mdl2.style.display="none"
  };
