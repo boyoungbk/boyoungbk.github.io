a0f4a78488cda0bef39055ec7c74acd1378034bd
boyoungbk <boyoungbk@gmail.com>
2022-07-18 14:55:07 +0900

    pop

--- js/kakaotalk.js
+++ js/kakaotalk.js
@@ -1,11 +1,4 @@
 function shareMessage() {
   Kakao.Share.sendCustom({
-    templateId: 79815,
-    templateArgs: {
-      title:
-        '현재보영 모바일청첩장',
-      description:
-        '2022.10.23. 일요일 12:30 드레스가든',
-    },
-  })
-}
+    templateId: 79815
+})};
