c21ed1259935cfab601a1c127e674e345bf19f4c
boyoungbk <boyoungbk@gmail.com>
2022-07-18 20:50:25 +0900

    kakao

--- js/kakaotalk.js
+++ js/kakaotalk.js
@@ -1,8 +1,10 @@
-if (!Kakao.isInitialized()) {
-  Kakao.init('a6bcf5f5770e250e68f1d08999c7fb4e')
-}
-
 function shareMessage() {
+  try {
+    if (Kakao) {
+        Kakao.init('a6bcf5f5770e250e68f1d08999c7fb4e');
+    };
+} catch(e) {};
+
   Kakao.Share.sendCustom({
     templateId: 79815
   })
