d047dd741bc48db4cbf24914e3ea9a47686ce76b
boyoungbk <boyoungbk@gmail.com>
2022-07-19 11:38:09 +0900

    { once : true}

--- js/height.js
+++ js/height.js
@@ -2,7 +2,7 @@ const appHeight = () => {
   const doc = document.documentElement
   doc.style.setProperty('--app-height', `${window.innerHeight}px`)
 }
-window.addEventListener('resize', appHeight)
+window.addEventListener('resize', appHeight, { once : true})
 appHeight()
 
 //첫 로드시에만 동작하게 바꾸기
