aa82fc2998dd13ecf4e5cab9b21fb13abd5a829b
boyoungbk <boyoungbk@gmail.com>
2022-07-21 23:50:14 +0900

    scroll

--- js/scroll.js
+++ js/scroll.js
@@ -9,20 +9,6 @@ function enableScroll(){
   document.body.removeEventListener('touchmove', preventDefault);
 }
 
-
-//텍스트 페이드인 출처: https://codepen.io/Madbones3/details/yLeRwdd
-
-const reveal = gsap.utils.toArray('.reveal');
-reveal.forEach((text, i) => {
-  ScrollTrigger.create({
-    trigger: text,
-    toggleClass: 'active',
-    start: 'top center',
-    ends: 'top 30%',
-    // markers: true
-  })
-})
-
 //텍스트 하이라이트 출처: https://codepen.io/ioiocodepen/pen/bPwzKo
 
 highlight();
@@ -35,7 +21,7 @@ function highlight(){
   var scroll = document.querySelector(window).scrollTop;
   var height = document.querySelector(window).height();
 
-  document.getElementsByClassName(".highlight").each(function(){
+  document.getElementsByClassName("highlight").each(function(){
     var pos = document.querySelector(this).offset().top;
     if (scroll+height >= pos) {
       document.querySelector(this).classList.add("active");
