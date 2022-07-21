cfaefb6223652da772ca0cb18c983386b7082124
boyoungbk <boyoungbk@gmail.com>
2022-07-21 11:35:08 +0900

    app

--- js/height.js
+++ js/height.js
@@ -9,10 +9,10 @@ const appHeight = () => {
 //   doc.style.setProperty('--app-height', app_height)
 // }
 
-// window.addEventListener('resize', appHeight, { once : true}) //첫 로드시에만 동작하게 바꾸기
-// appHeight()
+window.addEventListener('resize', appHeight, { once : true}) //첫 로드시에만 동작하게 바꾸기
+appHeight()
 
-if (navigator.userAgent.match(/like Mac OS X/i)) {
-  window.addEventListener('resize', appHeight, { once : true}) //첫 로드시에만 동작하게 바꾸기
-  appHeight()
-}
+// if (navigator.userAgent.match(/like Mac OS X/i)) {
+//   window.addEventListener('resize', appHeight, { once : true}) //첫 로드시에만 동작하게 바꾸기
+//   appHeight()
+// }
