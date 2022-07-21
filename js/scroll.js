27cb61a466873358385a88910b072dfed6ac095a
boyoungbk <boyoungbk@gmail.com>
2022-07-21 23:20:15 +0900

    scroll

--- js/scroll.js
+++ js/scroll.js
@@ -8,3 +8,45 @@ function disableScroll(){
 function enableScroll(){
   document.body.removeEventListener('touchmove', preventDefault);
 }
+
+
+//텍스트 페이드인 출처: https://codepen.io/Madbones3/details/yLeRwdd
+
+const reveal = gsap.utils.toArray('.reveal');
+reveal.forEach((text, i) => {
+  ScrollTrigger.create({
+    trigger: text,
+    toggleClass: 'active',
+    start: 'top center',
+    ends: 'top 30%',
+    // markers: true
+  })
+})
+
+//텍스트 하이라이트 출처: https://codepen.io/ioiocodepen/pen/bPwzKo
+
+highlight();
+
+document.querySelector(window).addEventListener("scroll", function(){
+  highlight();
+});
+
+function highlight(){
+  var scroll = document.querySelector(window).scrollTop;
+  var height = document.querySelector(window).height();
+
+  document.getElementsByClassName(".highlight").each(function(){
+    var pos = document.querySelector(this).offset().top;
+    if (scroll+height >= pos) {
+      document.querySelector(this).classList.add("active");
+    }
+    else
+    {
+      document.querySelector(this).classList.remove("active");
+      document.querySelector(this).classList.add("disactivated");
+    }
+    console.log(pos);
+    console.log(scroll);
+  });
+}  
+
