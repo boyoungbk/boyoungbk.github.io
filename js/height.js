acc5ff40a55a7d682b884931f71f502314730049
boyoungbk <boyoungbk@gmail.com>
2022-07-21 11:16:32 +0900

    제이쿼리까지..

--- js/height.js
+++ js/height.js
@@ -1,13 +1,13 @@
-// const appHeight = () => {
-//   const doc = document.documentElement
-//   doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-// }
-
 const appHeight = () => {
   const doc = document.documentElement
-  const app_height = document.querySelector("window.innerHeight") + "px"
-  doc.style.setProperty('--app-height', app_height)
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
 }
+
+// const appHeight = () => {
+//   const doc = document.documentElement
+//   const app_height = document.querySelector("window.innerHeight") + "px"
+//   doc.style.setProperty('--app-height', app_height)
+// }
 
 window.addEventListener('resize', appHeight, { once : true}) //첫 로드시에만 동작하게 바꾸기
 appHeight()
