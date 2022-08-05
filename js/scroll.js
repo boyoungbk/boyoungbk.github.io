a8a44e9600d566211ea4f482ccb221c72566ae9e
boyoungbk <boyoungbk@gmail.com>
2022-08-05 12:00:26 +0900

    확대금지

--- js/scroll.js
+++ js/scroll.js
@@ -27,3 +27,18 @@ function enableScroll(){
   });
 })(window, document);
 
+document.documentElement.addEventListener('touchstart', function (event) {
+  if (event.touches.length > 1) {
+       event.preventDefault(); 
+     } 
+ }, false);
+
+var lastTouchEnd = 0; 
+
+document.documentElement.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+       event.preventDefault(); 
+     } lastTouchEnd = now; 
+ }, false);
+ 
