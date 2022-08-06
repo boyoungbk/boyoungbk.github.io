6f9f83512ddf70dc467e37a0d147998444e91b2f
DESKTOP-D1IHBH1\boyoungbk <boyoungbk@gmail.com>
2022-08-06 15:41:13 +0900

    calendar

--- js/dday.js
+++ js/dday.js
@@ -1,6 +1,7 @@
 const today = new Date();
-const dday= new Date(2022, 9, 23);
+const dday = new Date(2022, 9, 23);
 const gap = dday.getTime() - today.getTime();
 const result = Math.ceil(gap / (1000 * 60 * 60 * 24));
 
 document.getElementById("dday").textContent = "D-" + result;
+document.getElementById("dday2").textContent = result;
