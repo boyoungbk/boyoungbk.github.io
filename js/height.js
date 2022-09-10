43f24bb59f3c2421035be8799a6330a328e7d39a
boyoungbk <boyoungbk@gmail.com>
2022-09-10 14:18:05 +0900

    height

--- js/height.js
+++ js/height.js
@@ -1,6 +1,8 @@
 const appHeight = () => {
   const doc = document.documentElement
-  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+  const height = window.innerHeight
+  doc.style.setProperty('--app-height', height + "px")
+  doc.style.setProperty('--app-top', height * 0.8 + "px")
 }
 
 window.addEventListener('load', appHeight, {once : true})
