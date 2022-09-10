06506f4b9745639d1d1f218f3099af5ccc4f33b9
boyoungbk <boyoungbk@gmail.com>
2022-09-10 12:58:31 +0900

    인포

--- js/height.js
+++ js/height.js
@@ -1,7 +1,6 @@
 const appHeight = () => {
   const doc = document.documentElement
   doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-  doc.style.setProperty('--app-bottom', 3+"rem")
 }
 
 window.addEventListener('load', appHeight, {once : true})
